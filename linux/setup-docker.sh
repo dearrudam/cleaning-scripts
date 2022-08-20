8945f941ebf94f17c23eb94f70c77752616d4deb
Maximillian Arruda <dearrudam@gmail.com>
2022-08-20 07:47:04 +0000

    Installing docker-compose

--- linux/setup-docker.sh
+++ linux/setup-docker.sh
@@ -40,3 +40,8 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 docker run hello-world
+
+# docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
