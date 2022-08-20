b42cc4268fe6dfd547591521f3aa27ef328ce970
Maximillian Arruda <dearrudam@gmail.com>
2022-08-20 06:04:56 +0000

    updated

--- linux/setup-pip.sh
+++ linux/setup-pip.sh
@@ -4,6 +4,8 @@ pip3 --version
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
+pip install -U pytest
+
 source $HOME/.poetry/env
 
 echo "source \$HOME/.poetry/env" >> $HOME/.bashrc
