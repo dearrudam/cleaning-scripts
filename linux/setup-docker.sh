fc230f74bac6136c57580adf9a881e7d94c4693e
Maximillian Arruda <dearrudam@gmail.com>
2022-08-20 07:54:09 +0000

    Change permission

--- linux/setup-docker.sh
+++ linux/setup-docker.sh
@@ -45,3 +45,5 @@ docker run hello-world
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
