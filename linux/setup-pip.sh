dbe5b580b8edccddf94645398a3051301bcc30c5
Maximillian Arruda <dearrudam@gmail.com>
2022-08-20 05:50:11 +0000

    updated

--- linux/setup-pip.sh
+++ linux/setup-pip.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
 sudo apt-get install -y python3-pip python3.8-venv
 pip3 --version
+
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+source $HOME/.poetry/env
 poetry --version
+
+alias python='python3'
