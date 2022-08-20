5f5050020a42ba90de367c597b3df38696fc59cb
Maximillian Arruda <dearrudam@gmail.com>
2022-08-20 05:46:19 +0000

    update

--- linux/setup-pip.sh
+++ linux/setup-pip.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip python3.8-venv
 pip3 --version
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+poetry --version
